445e0314fae9f2d8dd95873d254e2d96c26082f2
BRHGreen <benedictgreen@gmail.com>
2017-10-12 21:29:12 +0100

    rendering first component

--- gello-world/src/index.js
+++ gello-world/src/index.js
@@ -1,8 +1,21 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import Routes from './routes'
+import ReactDOM from 'react-dom'
+import { ApolloClient, createNetworkInterface, ApolloProvider } from 'react-apollo'
+
+import 'antd/dist/antd.css';
+
+const networkInterface = createNetworkInterface({
+  uri: 'http://localhost:3000/graphql'
+});
+const client = new ApolloClient({
+  networkInterface: networkInterface
+})
+
+const App = () => (
+  <ApolloProvider client={client}>
+    <Routes />
+  </ApolloProvider>
+)
 
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
